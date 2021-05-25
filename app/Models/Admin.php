c928dccee445febad6d13ee3eb5c9c7587bbeec5
Nazia Shehnaz <ncoder76@gmail.com>
2021-05-25 12:32:02 +0600

    first commit

---
+++ app/Models/Admin.php
@@ -0,0 +1,22 @@
+ // app/Admin.php
+ <?php
+
+    namespace App;
+
+    use Illuminate\Notifications\Notifiable;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+
+    class Admin extends Authenticatable
+    {
+        use Notifiable;
+
+        protected $guard = 'admin';
+
+        protected $fillable = [
+            'name', 'email', 'password',
+        ];
+
+        protected $hidden = [
+            'password', 'remember_token',
+        ];
+    }
---
+++ app/Models/Writer.php
@@ -0,0 +1,22 @@
+// app/Writer.php
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Writer extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guard = 'writer';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
