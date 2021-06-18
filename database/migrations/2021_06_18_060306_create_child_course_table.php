e34851e192d539261ad6c4c96dbe770a3f41f8b3
Fairooz Nawar <fairooznawar464@gmail.com>
2021-06-18 12:46:42 +0600

    Child_course many to many

---
+++ database/migrations/2021_06_18_060306_create_child_course_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChildCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('child_course', function (Blueprint $table) {
+            $table->increments('child_course_id');
+            $table->integer('child_id')->unsigned()->nullable();
+            $table->foreign('child_id')
+                ->references('child_id')->on('childs')
+                ->onDelete('set null');
+            $table->string('course_code')->nullable();
+            $table->foreign('course_code')
+                ->references('course_code')->on('courses')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('child_course');
+    }
+}
